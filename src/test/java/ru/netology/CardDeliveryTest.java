cfc825671606516253084ca6a3ee1913db2ef947
Irina Rogova <buyersbirds@gmail.com>
2021-01-27 12:08:49 +0600

    changed tests

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,9 +1,11 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -13,12 +15,17 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+
+
+    @BeforeEach
+    void setUpAll() {
+        open("http://localhost:9999");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+    }
+
     @Test
     void shouldSendIfAllFieldIsCorrect() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Брянск");
-
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         LocalDate dateOfDelivery = LocalDate.now().plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String date = formatter.format(dateOfDelivery);
@@ -27,15 +34,15 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+77078397070");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(byText("Успешно!")).waitUntil(visible, 14000);
-        $("[data-test-id=notification] .notification__content").shouldHave(text("Встреча успешно забронирована на "+date));
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofMillis(14000));
+        $("[data-test-id=notification] .notification__content").shouldHave
+                (text("Встреча успешно забронирована на "+date));
     }
 
     @Test
     void shouldNotSendIfCityIsNotCorrect() {
-        open("http://localhost:9999/");
+
         $("[data-test-id='city'] input").setValue("Алматы");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         LocalDate dateOfDelivery = LocalDate.now().plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String date = formatter.format(dateOfDelivery);
@@ -45,15 +52,15 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+77078397070");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна"));
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave
+                (exactText("Доставка в выбранный город недоступна"));
 
     }
 
     @Test
     void shouldNotSendIfDateIsNotCorrect() {
-        open("http://localhost:9999/");
+
         $("[data-test-id='city'] input").setValue("Брянск");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         LocalDate dateOfDelivery = LocalDate.now().plusDays(2);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String date = formatter.format(dateOfDelivery);
@@ -63,15 +70,15 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+77078397070");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave
+                (exactText("Заказ на выбранную дату невозможен"));
 
     }
 
     @Test
     void shouldNotSendIfNameIsNotCorrect() {
-        open("http://localhost:9999/");
+
         $("[data-test-id='city'] input").setValue("Брянск");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         LocalDate dateOfDelivery = LocalDate.now().plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String date = formatter.format(dateOfDelivery);
@@ -81,15 +88,15 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+77078397070");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
 
     }
 
     @Test
     void shouldNotSendIfTelIsNotCorrect() {
-        open("http://localhost:9999/");
+
         $("[data-test-id='city'] input").setValue("Брянск");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         LocalDate dateOfDelivery = LocalDate.now().plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String date = formatter.format(dateOfDelivery);
@@ -99,15 +106,15 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+7707");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText
+                ("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 
     @Test
     void shouldNotSendIfAgreementIsNotCorrect() {
-        open("http://localhost:9999/");
+
         $("[data-test-id='city'] input").setValue("Брянск");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         LocalDate dateOfDelivery = LocalDate.now().plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String date = formatter.format(dateOfDelivery);
@@ -117,17 +124,19 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+77078397070");
 
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText
+                ("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
 
     }
 
 
     @Test
     void shouldNotSendIfTestIsEmpty() {
-        open("http://localhost:9999/");
+
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(exactText
+                ("Поле обязательно для заполнения"));
 
     }
 
